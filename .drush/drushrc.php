d8bbe29bf7b48565f6306715508303198b179708
Hiber Shi <hiber@Hibers-MBP.lan>
2014-12-18 16:45:24 +0800

    init drush

---
+++ .drush/drushrc.php
@@ -0,0 +1,4 @@
+<?php
+ini_set('memory_limit','256M');
+$command_specific['site-install'] = array('account-name' => 'admin', 'account-pass' => 'admin');
+$command_specific['archive-restore'] = array('db-su' => 'root', 'db-su-pw' => '');
