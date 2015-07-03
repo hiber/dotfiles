9218731ae5e30074f445fbd252a3912b52432524
hiber <hiber@hibersh.com>
2015-07-03 12:38:02 +0800

    fontset han PingFang

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -31,7 +31,8 @@
 (global-set-key (kbd "C-+") 'text-scale-increase)
 (global-set-key (kbd "C--") 'text-scale-decrease)
 (global-set-key (kbd "C-c C-k") 'compile)
-
+(set-fontset-font "fontset-default"
+		  'han '("PingFang SC" . "unicode-bmp"))
 
 (eval-when-compile
   (require 'use-package))
