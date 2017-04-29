d41e8e5537d4054c3b41c40215b84567ef7411d5
Hiber Shi <hiber@hibersh.com>
2017-04-29 18:08:40 +0800

    spacemacs layers

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -31,24 +31,38 @@ values."
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
    '(
-     ;; ----------------------------------------------------------------
-     ;; Example of useful layers you may want to use right away.
-     ;; Uncomment some layer names and press <SPC f e R> (Vim style) or
-     ;; <M-m f e R> (Emacs style) to install them.
-     ;; ----------------------------------------------------------------
+     ;; Checkers
+     syntax-checking
+
+     ;; Completion
+     auto-completion
      helm
-     ;; auto-completion
-     ;; better-defaults
+
+     ;; Emacs
+     better-defaults
+
+     ;; Programming and markup languages
      emacs-lisp
-     ;; git
-     ;; markdown
-     ;; org
-     ;; (shell :variables
-     ;;        shell-default-height 30
-     ;;        shell-default-position 'bottom)
-     ;; spell-checking
-     ;; syntax-checking
-     ;; version-control
+     html
+     javascript
+     (markdown :variables markdown-live-preview-engine 'vmd)
+     php
+
+     ;; Source control
+     git
+     github
+     version-control
+
+     ;; Tags
+     gtags
+
+     ;; Tools
+     (dash
+      :variables
+      helm-dash-docset-newpath "~/Library/Application Support/Dash/DocSets")
+     (shell :variables
+            shell-default-height 30
+            shell-default-position 'bottom)
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
